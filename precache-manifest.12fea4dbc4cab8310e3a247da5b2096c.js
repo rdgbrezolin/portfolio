d481184ac40453d7b2e9a61e2eaab1455f01fe4e
rdgbrezolin <brezolinr@gmail.com>
2021-06-04 13:01:45 -0300

    Updates

---
+++ precache-manifest.12fea4dbc4cab8310e3a247da5b2096c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e444db7bcaf757414d0bc6ee9c26b90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83dc9c34ecb05c70616d",
+    "url": "/static/css/main.b95c9c3b.chunk.css"
+  },
+  {
+    "revision": "8c54be10dcf05c272ff7",
+    "url": "/static/js/2.ea96fa96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea96fa96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83dc9c34ecb05c70616d",
+    "url": "/static/js/main.e4495067.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c59e5c4fa944205711d469d9cedb5a66",
+    "url": "/static/media/loja.c59e5c4f.png"
+  },
+  {
+    "revision": "a3a67a5c99c17b443a0bb8bf18a68570",
+    "url": "/static/media/palmeiras.a3a67a5c.png"
+  },
+  {
+    "revision": "131d1347685cf14be703cf7ea0e47845",
+    "url": "/static/media/profile.131d1347.jpeg"
+  },
+  {
+    "revision": "6bb4927b33ee33c32fe060242c98945c",
+    "url": "/static/media/receitaLogin.6bb4927b.png"
+  },
+  {
+    "revision": "97b148ab94cc6510cf041710f5dcc347",
+    "url": "/static/media/receitas2.97b148ab.png"
+  },
+  {
+    "revision": "3fcd26c85f00cab19b472fad334db83c",
+    "url": "/static/media/receitas3.3fcd26c8.png"
+  },
+  {
+    "revision": "a5ba3c3115443f9847d2f77b6d9eb40b",
+    "url": "/static/media/trivia2.a5ba3c31.png"
+  },
+  {
+    "revision": "40ada3969b304f2b18919f5c3afa3cfd",
+    "url": "/static/media/trivia3.40ada396.png"
+  },
+  {
+    "revision": "bb12f65c19de0cec874a7b393cdbf644",
+    "url": "/static/media/trivialogin.bb12f65c.png"
+  }
+]);
