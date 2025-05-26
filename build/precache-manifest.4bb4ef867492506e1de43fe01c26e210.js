9deda94810a0557202cbca3a3af64f9429f9c46a
Rodrigo Brezolin <rodrigobrezolin@MacBook-Air-de-Rodrigo.local>
2025-05-26 16:08:53 -0300

    update portfolio

---
+++ build/precache-manifest.4bb4ef867492506e1de43fe01c26e210.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdf5341aa1621630fabf2d703dfd80c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6d832ee8f593e00cdc39",
+    "url": "/portfolio/static/css/main.80459bd5.chunk.css"
+  },
+  {
+    "revision": "04b05ef1c25f77192204",
+    "url": "/portfolio/static/js/2.a1719438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a1719438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d832ee8f593e00cdc39",
+    "url": "/portfolio/static/js/main.6c93e04e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "36abb698bae599b6b77c2467b4899b1f",
+    "url": "/portfolio/static/media/Avatar.36abb698.png"
+  },
+  {
+    "revision": "78739a9dc3cce981685f521e46d621f8",
+    "url": "/portfolio/static/media/logonew.78739a9d.png"
+  },
+  {
+    "revision": "c59e5c4fa944205711d469d9cedb5a66",
+    "url": "/portfolio/static/media/loja.c59e5c4f.png"
+  },
+  {
+    "revision": "a3a67a5c99c17b443a0bb8bf18a68570",
+    "url": "/portfolio/static/media/palmeiras.a3a67a5c.png"
+  },
+  {
+    "revision": "6bb4927b33ee33c32fe060242c98945c",
+    "url": "/portfolio/static/media/receitaLogin.6bb4927b.png"
+  },
+  {
+    "revision": "97b148ab94cc6510cf041710f5dcc347",
+    "url": "/portfolio/static/media/receitas2.97b148ab.png"
+  },
+  {
+    "revision": "3fcd26c85f00cab19b472fad334db83c",
+    "url": "/portfolio/static/media/receitas3.3fcd26c8.png"
+  },
+  {
+    "revision": "a5ba3c3115443f9847d2f77b6d9eb40b",
+    "url": "/portfolio/static/media/trivia2.a5ba3c31.png"
+  },
+  {
+    "revision": "40ada3969b304f2b18919f5c3afa3cfd",
+    "url": "/portfolio/static/media/trivia3.40ada396.png"
+  },
+  {
+    "revision": "bb12f65c19de0cec874a7b393cdbf644",
+    "url": "/portfolio/static/media/trivialogin.bb12f65c.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12fea4dbc4cab8310e3a247da5b2096c.js"
+  "/portfolio/precache-manifest.4bb4ef867492506e1de43fe01c26e210.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
